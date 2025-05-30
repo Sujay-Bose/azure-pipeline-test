6b47aec8598e2a18a0d1d8252f9fbdedc91d54d4
Sujay Bose <sbose@presidio.com>
2025-05-30 19:20:36 +0530

    added code for creating M365 group in Entra ID

--- main.tf
+++ main.tf
@@ -2,3 +2,14 @@ resource "azurerm_resource_group" "resource_group" {
   name     = var.RESOURCE_GROUP_NAME
   location = "East US"
 }
+
+resource "azuread_group" "owner_group" {
+  display_name     = var.dl_display_name 
+  types            = ["Unified"]
+  security_enabled = true
+  mail_enabled     = true
+  external_senders_allowed = true
+  mail_nickname    = var.dl_display_name 
+  description      = "DL containing owners of a project in Wiz"
+  owners           = [data.azuread_user.owner_user.id] 
+}
--- variables.tf
+++ variables.tf
@@ -2,3 +2,8 @@ variable "RESOURCE_GROUP_NAME" {
   type = string
   # default = "test"
 }
+
+variable "dl_display_name" {
+  type = string
+  description = "Display name of the DL"
+}
